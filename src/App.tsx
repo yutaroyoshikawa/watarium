a3d86f57abd65cfe3c1cb59b75a876a196ab0ef3
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-12-25 23:28:02 +0900

    メニュー作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,68 @@
-import React from 'react';
-import { createGlobalStyle } from 'styled-components';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import styled, { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import Menu from './components/Menu';
 
 const App: React.FC = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
+
   return (
-    <>
+    <Router>
       <GlobalStyle />
-      <header>
-        <figure>
+      <Header>
+        <Logo to="/">
           <img src={`${process.env.PUBLIC_URL}/img/watarium_logo.gif`} alt="ロゴ" />
-        </figure>
-      </header>
-    </>
+        </Logo>
+        <Menu isOpen={isOpenMenu} onSwitch={() => setIsOpenMenu(!isOpenMenu)} />
+        <PageName>展示一覧</PageName>
+      </Header>
+    </Router>
   );
 }
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap');
   ${reset}
+  h3, p, a {
+    font-family: 'Noto Sans JP', sans-serif;
+    font-size: 18px;
+  }
+  a {
+    text-decoration: none;
+    color: #707070;
+    transition: color .2s ease;
+    &:hover {
+      color: #202020;
+    }
+  }
 `;
+
+const Header = styled.header`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  top: 0;
+  left: 61px;
+  z-index:1000;
+  height: 100vh;
+`;
+
+const Logo = styled(Link)`
+  position: fixed;
+  top: 52px;
+  left: 61px;
+  z-index: 1000;
+  height: 55px;
+`;
+
+const PageName = styled.h3`
+  font-size: 15px;
+  color: #707070;
+  display: flex;
+  align-items: flex-start;
+`
 
 export default App;
---
+++ src/components/Menu.tsx
@@ -0,0 +1,160 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { CSSTransition } from 'react-transition-group';
+import { TransitionStatus } from 'react-transition-group/Transition';
+import { Link } from 'react-router-dom';
+
+const TRANSITION_DURATION = 300;
+
+interface LinkItem {
+  label: string;
+  url: string;
+}
+
+interface Props {
+  isOpen: boolean;
+  onSwitch: () => void;
+}
+
+const links: LinkItem[] = [
+  {
+    label: 'スケジュール',
+    url: 'schedule'
+  },
+  {
+    label: '展覧会',
+    url: 'exhibitions'
+  },
+  {
+    label: 'メンバーシップ',
+    url: 'membership'
+  },
+  {
+    label: 'ワタリウム美術館について',
+    url: 'about'
+  }
+];
+
+const Menu: React.FC<Props> = props => {
+  return (
+    <>
+      <IconBars onClick={props.onSwitch} isOpen={props.isOpen} />
+      <CSSTransition in={props.isOpen} timeout={TRANSITION_DURATION}>
+        {state => (
+          <MenuWrap state={state}>
+            <MenuList>
+              {
+                links.map(item => (
+                  <MenuItem key={item.url}>
+                    <Link to={`/${item.url}`}>
+                      {item.label}
+                    </Link>
+                  </MenuItem>
+                ))
+              }
+            </MenuList>
+          </MenuWrap>
+        )}
+      </CSSTransition>
+    </>
+  )
+};
+
+interface MenuWrap {
+  state: TransitionStatus
+}
+
+const MenuWrap = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 999;
+  background-color: #fff;
+
+  ${(props: MenuWrap) => {
+    switch (props.state) {
+      case 'entering':
+        return css`
+          opacity: 0;
+        `
+      case 'entered':
+        return css`
+          opacity: 1;
+          transition: opacity ${TRANSITION_DURATION}ms ease;
+        `
+      case 'exiting':
+        return css`
+          opacity: 1;
+        `
+      case 'exited':
+        return css`
+          opacity: 0;
+          transition: opacity ${TRANSITION_DURATION}ms ease;
+        `
+    }
+  }}
+`;
+
+const MenuList = styled.ul`
+  margin-right: 204px;
+`;
+
+const MenuItem = styled.li`
+  font-size: 20px;
+  color: #707070;
+
+  &:not(:last-child) {
+    margin-bottom: 80px;
+  }
+`;
+
+const bar = css`
+  content: '';
+  display: block;
+  height: 2px;
+  background-color: #707070;
+  transform-origin: center left;
+  transition: width .2s ease-out, transform .2s ease-out;
+`;
+
+interface Bars {
+  isOpen: boolean;
+}
+
+const IconBars = styled.div`
+  width: 58px;
+  height: 21px;
+  margin: 47px 0;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: space-between;
+  cursor: pointer;
+  position: relative;
+  z-index: 1000;
+
+  &::before {
+    ${bar}
+    transform: ${(props: Bars) => props.isOpen && 'rotate(20deg)'};
+    width: 58px;
+  }
+
+  &::after {
+    ${bar}
+    width: ${(props: Bars) => props.isOpen ? '58px' : '35px'};
+    transform: ${(props: Bars) => props.isOpen && 'rotate(-20deg)'};
+  }
+
+  &:hover {
+    &:after {
+      width: 58px;
+    }
+  }
+`;
+
+export default Menu;
