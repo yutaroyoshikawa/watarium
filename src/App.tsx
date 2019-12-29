3e1cda298f435dd98a1bedcbd894b7ee5e9635cd
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-12-29 10:08:36 +0900

    ページルーティング

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,93 @@
-import React from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
-import styled, { createGlobalStyle } from 'styled-components';
-import reset from 'styled-reset';
-import Menu from './components/Menu';
-import CurrentName from './components/CurrentName';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route,
+  useLocation
+} from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { TransitionStatus } from "react-transition-group/Transition";
+import styled, { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import Menu from "./components/Menu";
+import CurrentName from "./components/CurrentName";
+import About from "./pages/About";
+import Membership from "./pages/Membership";
 
-export type Labels = 'スケジュール' | '展覧会' | 'メンバーシップ' | 'ワタリウム美術館について';
-export type Urls = 'schedule' | 'exhibitions' | 'membership' | 'about';
+export type Labels =
+  | "スケジュール"
+  | "展覧会"
+  | "メンバーシップ"
+  | "ワタリウム美術館について";
+export type Urls = "schedule" | "exhibitions" | "membership" | "about";
+
+export interface TransitionProp {
+  transitionStatus: TransitionStatus;
+  duration: number
+}
 
 interface LinkItem {
   label: Labels;
   url: Urls;
 }
 
+interface PageItem {
+  url: Urls;
+  component: React.FC<any>;
+}
+
 export const links: LinkItem[] = [
   {
-    label: 'スケジュール',
-    url: 'schedule'
+    label: "スケジュール",
+    url: "schedule"
   },
   {
-    label: '展覧会',
-    url: 'exhibitions'
+    label: "展覧会",
+    url: "exhibitions"
   },
   {
-    label: 'メンバーシップ',
-    url: 'membership'
+    label: "メンバーシップ",
+    url: "membership"
   },
   {
-    label: 'ワタリウム美術館について',
-    url: 'about'
+    label: "ワタリウム美術館について",
+    url: "about"
   }
 ];
+
+const pages: PageItem[] = [
+  {
+    url: "about",
+    component: About
+  },
+  {
+    url: "membership",
+    component: Membership
+  }
+];
+
+export const TRANSITION_DURATION = 300;
+
+const PageSwicher: React.FC = () => {
+  const location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.key} timeout={TRANSITION_DURATION}>
+        {status => (
+          <Switch>
+            {pages.map(page => (
+              <Route exact={true} path={`/${page.url}`} key={page.url}>
+                <page.component transitionStatus={status} />
+              </Route>
+            ))}
+          </Switch>
+        )}
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
 
 const App: React.FC = () => {
   return (
@@ -38,11 +95,17 @@ const App: React.FC = () => {
       <GlobalStyle />
       <Header>
         <Logo to="/">
-          <img src={`${process.env.PUBLIC_URL}/img/watarium_logo.gif`} alt="ロゴ" />
+          <img
+            src={`${process.env.PUBLIC_URL}/img/watarium_logo.gif`}
+            alt="ロゴ"
+          />
         </Logo>
         <Menu />
         <CurrentName />
       </Header>
+      <MainContent>
+        <PageSwicher />
+      </MainContent>
     </Router>
   );
 };
@@ -50,7 +113,7 @@ const App: React.FC = () => {
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap');
   ${reset}
-  h3, p, a {
+  h1, h2, h3, p, a, address, button, li, span, label, dd, dl, dt {
     font-family: 'Noto Sans JP', sans-serif;
     font-size: 18px;
   }
@@ -62,6 +125,9 @@ const GlobalStyle = createGlobalStyle`
       color: #202020;
     }
   }
+  img {
+    width: 100%;
+  }
 `;
 
 const Header = styled.header`
@@ -72,7 +138,7 @@ const Header = styled.header`
   justify-content: center;
   top: 0;
   left: 61px;
-  z-index:1000;
+  z-index: 1000;
   height: 100vh;
 `;
 
@@ -82,6 +148,11 @@ const Logo = styled(Link)`
   left: 61px;
   z-index: 1000;
   height: 55px;
+`;
+
+const MainContent = styled.main`
+  margin-left: 240px;
+  width: calc(100% - 240px);
 `;
 
 export default App;
